949fbb8618eb6025fd8d3af587a0325ff9e1590f
Cristi Sandu <cristi.sandu@csx.ro>
2025-10-15 12:20:47 +0300

    Initial Xcode project setup

---
+++ Noi2/Noi2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Noi2
+//
+//  Created by Cristi Sandu on 15.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noi2/Noi2/Noi2App.swift
@@ -0,0 +1,17 @@
+//
+//  Noi2App.swift
+//  Noi2
+//
+//  Created by Cristi Sandu on 15.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Noi2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
