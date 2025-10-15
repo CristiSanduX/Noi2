a28e696c169b2c905059045ce14649d8deb60ed4
Cristi Sandu <cristi.sandu@csx.ro>
2025-10-15 22:45:13 +0300

    Add Firebase setup

--- Noi2/Noi2/Noi2App.swift
+++ Noi2/Noi2/Noi2App.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import UIKit
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Firebase configured")
+        return true
+    }
+}
 
 @main
 struct Noi2App: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { ContentView() }
     }
 }
